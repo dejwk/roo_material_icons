f559ce25b6ee2fdf67ed4f8b0bedfce2aaefb885
Dawid Kurzyniec <dawidk@google.com>
2021-06-18 16:24:30 +0200

    Removing the dedicated icon class, as the image now supports setting the color directly.

--- roo_material_icons.h
+++ roo_material_icons.h
@@ -6,22 +6,7 @@
 
 namespace roo_display {
 
-typedef RleImage4bppxBiased<Alpha4, PrgMemResource> MaterialIconDef;
-
-class MaterialIcon : public Drawable {
- public:
-  MaterialIcon(const MaterialIconDef& icon, Color color = color::Black)
-      : icon_(icon), color_(color) {}
-
-  Box extents() const override { return icon_.extents(); }
-
-  void setColor(Color color) { color_ = color; }
-
- private:
-  void drawTo(const Surface& s) const override;
-
-  const MaterialIconDef& icon_;
-  Color color_;
-};
+// A convenience typedef.
+typedef RleImage4bppxBiased<Alpha4, PrgMemResource> MaterialIcon;
 
 }  // namespace roo_display
